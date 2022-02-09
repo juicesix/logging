cbbc54413c6bde850006295d69d355c64733af61
juicesix <2358343165@qq.com>
2022-02-09 18:47:32 +0800

    add genlog

--- common_log.go
+++ common_log.go
@@ -38,3 +38,122 @@ func CloseCommonLog() {
 func OpenCommonLog() {
 	trunon = true
 }
+
+func checkOpenStatus() bool {
+	if trunon == false {
+		return false
+	}
+	return true
+}
+
+func checkNeedLog() bool {
+	if isInit == false {
+		return false
+	}
+
+	return true
+}
+
+func GenLog(v ...interface{}) {
+	if !checkOpenStatus() {
+		return
+	}
+
+	if !checkNeedLog() {
+		_defaultLogger.Debug(v...)
+		return
+	}
+
+	genlog.Debug(v...)
+}
+
+func GenLogf(format string, v ...interface{}) {
+
+	if !checkOpenStatus() {
+		return
+	}
+
+	if !checkNeedLog() {
+		_defaultLogger.Debugf(format, v...)
+		return
+	}
+
+	genlog.Debugf(format, v...)
+}
+
+func SlowLog(v ...interface{}) {
+
+	if !checkOpenStatus() {
+		return
+	}
+
+	if !checkNeedLog() {
+		_defaultLogger.Debug(v...)
+		return
+	}
+	slowlog.Debug(v...)
+}
+
+func SlowLogf(format string, v ...interface{}) {
+
+	if !checkOpenStatus() {
+		return
+	}
+
+	if !checkNeedLog() {
+		_defaultLogger.Debugf(format, v...)
+		return
+	}
+	slowlog.Debugf(format, v...)
+}
+
+func CrashLog(v ...interface{}) {
+
+	if !checkOpenStatus() {
+		return
+	}
+
+	if !checkNeedLog() {
+		_defaultLogger.Debug(v...)
+		return
+	}
+
+	crashlog.Debug(v...)
+}
+
+func CrashLogf(format string, v ...interface{}) {
+
+	if !checkOpenStatus() {
+		return
+	}
+
+	if !checkNeedLog() {
+		_defaultLogger.Debugf(format, v...)
+		return
+	}
+	crashlog.Debugf(format, v...)
+}
+
+func BalanceLog(v ...interface{}) {
+
+	if !checkOpenStatus() {
+		return
+	}
+	if !checkNeedLog() {
+		_defaultLogger.Debug(v...)
+		return
+	}
+	balancelog.Debug(v...)
+}
+
+func BalanceLogf(format string, v ...interface{}) {
+
+	if !checkOpenStatus() {
+		return
+	}
+	if !checkNeedLog() {
+		_defaultLogger.Debugf(format, v...)
+		return
+	}
+	balancelog.Debugf(format, v...)
+}
