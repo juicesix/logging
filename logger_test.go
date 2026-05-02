915b04f0551045fbe093003f81492988e76c634b
juicesix <2358343165@qq.com>
2026-05-02 15:24:32 +0800

    delete重复

--- logger_test.go
+++ logger_test.go
@@ -102,22 +102,4 @@ func TestMillSecondTimeEncoder_Second(t *testing.T) {
 
 // 增加一行注释
 
-// 针对秒级时间确认编码结果仍包含毫秒部分
-func TestMillSecondTimeEncoder_Second(t *testing.T) {
-	enc := &stringArrayEncoder{}
-	when := time.Date(2026, 4, 30, 9, 32, 45, 123456000, time.UTC)
-
-	MillSecondTimeEncoder(when, enc)
-
-	if len(enc.values) != 1 {
-		t.Fatalf("expected one encoded value, got %d", len(enc.values))
-	}
-	if enc.values[0] != "2026-04-30 09:32:45.123" {
-		t.Fatalf("unexpected encoded time: %q", enc.values[0])
-	}
-
-}
-
-// 增加一行注释
-
 // git stash test
